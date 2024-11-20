38cff2b47f88c6c2fcb715ce64b4f9442eed4d8b
unknown <your@email.example>
2024-11-20 15:50:31 +0200

    .\js-basice.js

---
+++ js-basice.js
@@ -0,0 +1,50 @@
+
+// let firstName = "Jānis";
+
+// console.log("Niklāvs");
+
+// let myArray = [1,2,3,4];
+
+// let x = 11;
+// if (x == 10){
+//     console.log("x ir vienāds par 10")
+// }
+
+// let person ={
+//     name: "Antra",
+//     age: 16,
+// }
+// console.log(person)
+
+// console.log(person)
+// person.vecums= 16;
+// console.log(person);
+
+// let ageStatus = person.vecums >=18 ? "Pilngadīgs" : "Nepilngadīgs";
+// console.log(ageStatus)
+
+
+// let namesArray =["Antra", "Ilmārs", "Renārs", "Pauls"]
+// // indeks ir attiecīgi [0,1,2,3]
+// namesArray[2];
+
+// for (let i = 0; i < namesArray.length; i++){
+//     console.log(namesArray[i]);
+
+// }
+
+
+// function drive() {
+//     console.log("dirving");
+// }
+// function getRecArea() {
+//     return 3 * 4;
+// }
+// let result = getRecArea();
+// cosole.log(result);
+
+function getRecArea(width, height) {
+    return width * height
+}
+console.log (getRecArea(5,6));
+
